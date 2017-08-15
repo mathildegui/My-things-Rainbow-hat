0d812c1d378122a5d0045e0f954ea67fff6df309
mathilde <mathilde.guillossou@gmail.com>
2017-08-15 21:40:48 +0200

    update project with ButtonInputDriver and key events implementation. Make each button (A-B-C) light the corresponding one (resp. Red-Green-Blue)

--- app/src/main/java/com/mathildeguillossou/myfirstthings/TouchActivity.kt
+++ app/src/main/java/com/mathildeguillossou/myfirstthings/TouchActivity.kt
@@ -2,18 +2,32 @@ package com.mathildeguillossou.myfirstthings
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
+import android.view.KeyEvent
+import com.google.android.things.contrib.driver.button.ButtonInputDriver
 import com.google.android.things.pio.Gpio
-import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManagerService
 import java.io.IOException
+import com.google.android.things.contrib.driver.button.Button
+
+
 
 class TouchActivity : Activity() {
 
+    private var redLED: Gpio?   = null
+    private var blueLED: Gpio?  = null
     private var greenLED: Gpio? = null
-    private var inputA: Gpio? = null
+
+    private var inputA: ButtonInputDriver?   = null
+    private var inputB: ButtonInputDriver?   = null
+    private var inputC: ButtonInputDriver?   = null
 
     private val TOUCH_BTN_A_PIN = "BCM21" //private static final String TOUCH_BTN_A_PIN = "BCM21";
+    private val TOUCH_BTN_B_PIN = "BCM20" //private static final String TOUCH_BTN_A_PIN = "BCM20";
+    private val TOUCH_BTN_C_PIN = "BCM16" //private static final String TOUCH_BTN_A_PIN = "BCM16";
+
+
+    private val RED_LED_PIN     = "BCM6"
+    private val BLUE_LED_PIN    = "BCM26"
     private val GREEN_LED_PIN   = "BCM19"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,80 +36,86 @@ class TouchActivity : Activity() {
         val service = PeripheralManagerService()
 
         try {
-            inputA = service.openGpio(TOUCH_BTN_A_PIN)
+            inputA = ButtonInputDriver(
+                    TOUCH_BTN_A_PIN,
+                    Button.LogicState.PRESSED_WHEN_LOW,
+                    KeyEvent.KEYCODE_0)
+            inputB = ButtonInputDriver(
+                    TOUCH_BTN_B_PIN,
+                    Button.LogicState.PRESSED_WHEN_LOW,
+                    KeyEvent.KEYCODE_1)
+            inputC = ButtonInputDriver(
+                    TOUCH_BTN_C_PIN,
+                    Button.LogicState.PRESSED_WHEN_LOW,
+                    KeyEvent.KEYCODE_2)
+
+            inputA?.register()
+            inputB?.register()
+            inputC?.register()
+
+            redLED   = service.openGpio(RED_LED_PIN)
+            blueLED  = service.openGpio(BLUE_LED_PIN)
             greenLED = service.openGpio(GREEN_LED_PIN)
         } catch (e: IOException) {
-            throw IllegalStateException(TOUCH_BTN_A_PIN + " bus cannot be opened.", e)
+            e.printStackTrace()
         }
 
         try {
-            inputA?.setDirection(Gpio.DIRECTION_IN)
-            inputA?.setActiveType(Gpio.ACTIVE_LOW)
-
+            redLED?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+            blueLED?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
             greenLED?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
-//            bus?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH)
-//            bus?.setActiveType(Gpio.ACTIVE_HIGH)
         } catch (e: IOException) {
-            throw IllegalStateException(TOUCH_BTN_A_PIN + " bus cannot be configured.", e)
+            e.printStackTrace()
         }
     }
 
 
-    private val mCallback = object : GpioCallback() {
-        override fun onGpioEdge(gpio: Gpio?): Boolean {
-            try {
-                val buttonValue = gpio!!.value
-                greenLED?.value = buttonValue
-            } catch (e: IOException) {
-                Log.w(TOUCH_BTN_A_PIN, "Error reading GPIO")
-            }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        setLed(keyCode, true)
+        return super.onKeyDown(keyCode, event)
+    }
 
-            // Return true to keep callback active.
-            return true
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        setLed(keyCode, false)
+        return super.onKeyUp(keyCode, event)
+    }
+
+    fun setLed(keyCode: Int, on: Boolean) {
+        when (keyCode) {
+            KeyEvent.KEYCODE_0 -> setLedValue(on, redLED)
+            KeyEvent.KEYCODE_1 -> setLedValue(on, greenLED)
+            KeyEvent.KEYCODE_2 -> setLedValue(on, blueLED)
         }
     }
 
-
-
-//    private val callback = object : GpioCallback() {
-//        override fun onGpioEdge(gpio: Gpio): Boolean {
-//            try {
-//                if (gpio.value) {
-//                    Log.i("TUT", "ON PRESSED DOWN")
-//                } else {
-//                    Log.i("TUT", "ON PRESSED UP")
-//                }
-//            } catch (e: IOException) {
-//                throw IllegalStateException(TOUCH_BTN_A_PIN + " cannot be read.", e)
-//            }
-//
-//            return true
-//        }
-//    }
-
-    override fun onStart() {
-        super.onStart()
+    /**
+     * Update the value of the LED output.
+     */
+    private fun setLedValue(value: Boolean, gpio: Gpio?) {
         try {
-            inputA?.setEdgeTriggerType(Gpio.EDGE_BOTH)
-            inputA?.registerGpioCallback(mCallback)
-            //inputA?.registerGpioCallback(callback)
+            gpio?.value = value
         } catch (e: IOException) {
-            throw IllegalStateException(TOUCH_BTN_A_PIN + " bus cannot be monitored.", e)
+            e.printStackTrace()
         }
     }
 
-    override fun onStop () {
-        super.onStop()
-        inputA?.unregisterGpioCallback(mCallback)
-//        inputA?.unregisterGpioCallback(callback)
-    }
 
     override fun onDestroy() {
         try {
+            inputA?.unregister()
             inputA?.close()
+
+            inputB?.unregister()
+            inputB?.close()
+
+            inputC?.unregister()
+            inputC?.close()
+
             greenLED?.close()
+            blueLED?.close()
+            redLED?.close()
         } catch (e: IOException) {
-            Log.e("TUT", TOUCH_BTN_A_PIN + " bus cannot be closed, you may experience errors on next launch.", e)
+            e.printStackTrace()
         }
 
         super.onDestroy()
